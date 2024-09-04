4ce9d707ddf06f23d840900b8acc027ac353a7d6
marshall-GK <code.multiosop@gmail.com>
2024-09-05 00:12:02 +0530

    api fixed

--- imagesList.js
+++ imagesList.js
@@ -2,41 +2,57 @@ const userStory = () => ({
   1: [
     {
       id: "1s1",
-      storyUrl: "https://picsum.photos/id/115/200/300",
-      timer: 3000,
+      storyUrl: "https://picsum.photos/id/115/640/480",
+      timer: 4800,
     },
   ],
   2: [
     {
       id: "2s1",
-      storyUrl: "https://picsum.photos/id/113/200/300",
+      storyUrl: "https://picsum.photos/id/113/640/480",
       timer: 8000,
     },
   ],
   3: [
     {
       id: "3s1",
-      storyUrl: "https://picsum.photos/id/870/200/300?grayscale&blur=2",
+      storyUrl: "https://picsum.photos/id/870/640/480?grayscale&blur=2",
       timer: 5000,
     },
-    { id: "3s2", storyUrl: "https://picsum.photos/id/112/200/300", timer: 7000 },
+    {
+      id: "3s2",
+      storyUrl: "https://picsum.photos/id/112/640/480",
+      timer: 7000,
+    },
   ],
   4: [
     {
       id: "4s1",
-      storyUrl: "https://picsum.photos/id/116/200/300",
+      storyUrl: "https://picsum.photos/id/116/640/480",
       timer: 1000,
     },
-    { id: "4s2", storyUrl: "https://picsum.photos/id/117/200/300", timer: 3000 },
+    {
+      id: "4s2",
+      storyUrl: "https://picsum.photos/id/117/640/480",
+      timer: 5000,
+    },
   ],
   5: [
     {
       id: "5s1",
-      storyUrl: "https://picsum.photos/id/121/200/300",
+      storyUrl: "https://picsum.photos/id/121/640/480",
       timer: 1000,
     },
-    { id: "5s2", storyUrl: "https://picsum.photos/id/120/200/300", timer: 3000 },
-    { id: "5s3", storyUrl: "https://picsum.photos/id/122/200/300", timer: 3000 },
+    {
+      id: "5s2",
+      storyUrl: "https://picsum.photos/id/120/640/480",
+      timer: 4800,
+    },
+    {
+      id: "5s3",
+      storyUrl: "https://picsum.photos/id/122/640/480",
+      timer: 4800,
+    },
   ],
 });
 
